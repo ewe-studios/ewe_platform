2d36b219ced9ee48d949b28fd11f23f2e47f3004
Alexander Ewetumo <trinoxf@gmail.com>
2024-05-26 00:15:10 +0000

    UPT: Add test for larger size

--- crates/html/src/bench.rs
+++ crates/html/src/bench.rs
@@ -18,7 +18,7 @@ impl Resetable for Dummy {
 
 #[bench]
 fn bench_dummy_usize_area_pool_with_drop(b: &mut Bencher) {
-    let limiter = MemoryLimiter::create_shared(calculate_size_for::<Dummy>(None) * 1024 * 1024);
+    let limiter = MemoryLimiter::create_shared(calculate_size_for::<Dummy>(None) * 8024 * 8024);
     let mut pool: ArenaPool<Dummy> = ArenaPool::new(limiter, || Dummy(0));
 
     b.iter(|| {
@@ -31,7 +31,7 @@ fn bench_dummy_usize_area_pool_with_drop(b: &mut Bencher) {
 
 #[bench]
 fn bench_dummy_usize_area_pool_with_deallocate(b: &mut Bencher) {
-    let limiter = MemoryLimiter::create_shared(calculate_size_for::<Dummy>(None) * 1024 * 1024);
+    let limiter = MemoryLimiter::create_shared(calculate_size_for::<Dummy>(None) * 8024 * 8024);
     let mut pool: ArenaPool<Dummy> = ArenaPool::new(limiter, || Dummy(0));
 
     b.iter(|| {
@@ -46,25 +46,22 @@ fn bench_dummy_usize_area_pool_with_deallocate(b: &mut Bencher) {
 struct DummyProfile {
     pub name: String,
     pub address: String,
-    pub weddings: Vec<String>,
 }
 
 impl Resetable for DummyProfile {
     fn reset(&mut self) {
         self.name.clear();
         self.address.clear();
-        self.weddings.clear();
     }
 }
 
 #[bench]
 fn bench_dummy_profile_area_pool_with_drop(b: &mut Bencher) {
     let limiter =
-        MemoryLimiter::create_shared(calculate_size_for::<DummyProfile>(None) * 1024 * 1024);
+        MemoryLimiter::create_shared(calculate_size_for::<DummyProfile>(None) * 8024 * 8024);
     let mut pool: ArenaPool<DummyProfile> = ArenaPool::new(limiter, || DummyProfile {
         name: String::from("alex"),
         address: String::from("New York"),
-        weddings: vec![String::from("north"), String::from("south")],
     });
 
     b.iter(|| {
@@ -79,12 +76,68 @@ fn bench_dummy_profile_area_pool_with_drop(b: &mut Bencher) {
 #[bench]
 fn bench_dummy_profile_area_pool_with_deallocate(b: &mut Bencher) {
     let limiter =
-        MemoryLimiter::create_shared(calculate_size_for::<DummyProfile>(None) * 1024 * 1024);
+        MemoryLimiter::create_shared(calculate_size_for::<DummyProfile>(None) * 8024 * 8024);
     let mut pool: ArenaPool<DummyProfile> = ArenaPool::new(limiter, || DummyProfile {
         name: String::from("alex"),
         address: String::from("New York"),
-        weddings: vec![String::from("north"), String::from("south")],
     });
+
+    b.iter(|| {
+        black_box({
+            let data = pool.allocate().expect("received handle");
+            data.element().borrow_mut().clone().unwrap().name = String::from("thunder");
+            data.deallocate();
+        })
+    })
+}
+
+#[derive(Clone)]
+struct DummyProfileWithWedding {
+    pub name: String,
+    pub address: String,
+    pub weddings: Vec<String>,
+}
+
+impl Resetable for DummyProfileWithWedding {
+    fn reset(&mut self) {
+        self.name.clear();
+        self.address.clear();
+        self.weddings.clear();
+    }
+}
+
+#[bench]
+fn bench_dummy_profile_with_wedding_area_pool_with_drop(b: &mut Bencher) {
+    let limiter = MemoryLimiter::create_shared(
+        calculate_size_for::<DummyProfileWithWedding>(None) * 8024 * 8024,
+    );
+    let mut pool: ArenaPool<DummyProfileWithWedding> =
+        ArenaPool::new(limiter, || DummyProfileWithWedding {
+            name: String::from("alex"),
+            address: String::from("New York"),
+            weddings: vec![String::from("north"), String::from("south")],
+        });
+
+    b.iter(|| {
+        black_box({
+            let data = pool.allocate().expect("received handle");
+            data.element().borrow_mut().clone().unwrap().name = String::from("thunder");
+            drop(data);
+        })
+    })
+}
+
+#[bench]
+fn bench_dummy_profile_with_wedding_area_pool_with_deallocate(b: &mut Bencher) {
+    let limiter = MemoryLimiter::create_shared(
+        calculate_size_for::<DummyProfileWithWedding>(None) * 8024 * 8024,
+    );
+    let mut pool: ArenaPool<DummyProfileWithWedding> =
+        ArenaPool::new(limiter, || DummyProfileWithWedding {
+            name: String::from("alex"),
+            address: String::from("New York"),
+            weddings: vec![String::from("north"), String::from("south")],
+        });
 
     b.iter(|| {
         black_box({
