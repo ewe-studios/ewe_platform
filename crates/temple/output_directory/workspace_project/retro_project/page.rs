86583d9eed47be8f8e3ae86bd99f3434034f7797
Alexander Ewetumo <trinoxf@gmail.com>
2024-10-23 23:35:37 +0800

    ADD: ignore files

--- crates/temple/output_directory/workspace_project/retro_project/page.rs
+++
@@ -1,7 +0,0 @@
-use tinytemplate::TinyTemplate;
-
-fn main() {
-    let mut tt = TinyTemplate::new();
-
-    println!("/home/darkvoid/Labs/ewestudios/ewe_platform/crates/temple/output_directory/workspace_project");
-}
