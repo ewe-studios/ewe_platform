7de8e8961e5942234a5ee9d77b8fc35e049d0738
Alexander Ewetumo <trinoxf@gmail.com>
2024-11-03 23:32:04 +0800

    ADD: add configuration loader

--- templates/http-app/src/bin/dev/main.rs
+++ templates/http-app/src/bin/dev/main.rs
@@ -18,35 +18,31 @@ async fn main() {
 
     ewe_trace::info!("Starting local binary");
 
-    let binary_name = String::from("wello");
+    let binary_name = std::env::var("BINARY_NAME")
+        .expect("should fetch BINARY_NAME from environment via .cargo/config.toml");
 
-    let project_directory = String::from("{{PROJECT_DIRECTORY}}");
-    let demo_directory = String::from(format!(
-        "{}/examples/web/{}",
-        &project_directory, binary_name,
-    ));
+    let project_directory = std::env::var("PROJECT_DIRECTORY")
+        .expect("should fetch PROJECT_DIRECTORY from environment via .cargo/config.toml");
 
-    let destination = ProxyRemoteConfig::new(String::from("0.0.0.0"), 3200);
     let source = ProxyRemoteConfig::new(String::from("0.0.0.0"), 3600);
+    let destination = ProxyRemoteConfig::new(String::from("0.0.0.0"), 3200);
 
     let tunnel_config = ProxyType::Http1(Http1::new(source, destination, Some(HashMap::new())));
 
     let definition = ProjectDefinition {
         proxy: tunnel_config,
-        watch_directory: demo_directory,
-        crate_name: binary_name,
+        crate_name: binary_name.clone(),
         workspace_root: project_directory.clone(),
+        watch_directory: project_directory.clone(),
         wait_before_reload: time::Duration::from_millis(300), // magic number that works
-        target_directory: String::from(format!("{}/target", project_directory)),
-        build_arguments: vec!["cargo", "build", "--bin", "wello"].to_vec_string(),
-        run_arguments: vec!["cargo", "run", "--bin", "wello"].to_vec_string(),
+        run_arguments: vec!["cargo", "run", "--bin", &binary_name].to_vec_string(),
+        build_arguments: vec!["cargo", "build", "--bin", &binary_name].to_vec_string(),
+        target_directory: String::from(format!("{}/target", project_directory.clone())),
     };
 
     let mut dev_service = HttpDevService::new(definition);
 
     let (_cancel_sender, cancel_receiver) = broadcast::channel::<()>(1);
-
-    // TODO: implement signal interrupt listener to send signal with _cancel_sender
 
     let waiter = dev_service
         .start(cancel_receiver)
