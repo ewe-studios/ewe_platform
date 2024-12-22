1cc8fc6dfdda82316446ea93c0049321b3d328c7
Alexander Ewetumo <trinoxf@gmail.com>
2024-12-22 15:46:19 +0800

    UPT: cleanup

--- backends/foundation_core/src/wire/event_source/mod.rs
+++ backends/foundation_core/src/wire/event_source/mod.rs
@@ -1,16 +1,12 @@
 extern crate url;
 
 mod error;
-pub use error::*;
 
 mod core;
-pub use core::*;
 
 #[cfg(not(target_arch = "wasm32"))]
 mod no_wasm;
 
-#[cfg(not(target_arch = "wasm32"))]
-pub use no_wasm::*;
 
 #[cfg(target_arch = "wasm32")]
 mod wasm;
