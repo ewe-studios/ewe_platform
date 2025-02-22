07990565536b5d9ea517c39576ab116e323321f7
Alexander Ewetumo <trinoxf@gmail.com>
2025-02-22 19:41:05 +0800

    ADD: platform to demo new work

--- backends/foundation_core/src/megatron/jsdom/mod.rs
+++ backends/foundation_core/src/megatron/jsdom/mod.rs
@@ -1,6 +1,41 @@
-use rust_embed::Embed;
+use rust_embed::{Embed, EmbeddedFile};
+
+use core::str;
+
+use crate::extensions::strings_ext::IntoString;
 
 #[derive(Embed)]
 #[folder = "src/megatron/jsdom/packages/"]
 #[prefix = "packages/"]
 pub struct Packages;
+
+/// [package_request_handler] provides a axum request handler that
+/// will serve static files from the [Packages] directory providing
+/// a way to use the underlying files from within it as part of
+/// your routes.
+/// It expects you to provide the name prefix the route wiill be coming
+/// with as that determines how we will swap that name to `packages/`.
+///
+/// i.e if the path incoming is /public/packer.js then the
+/// path will become /packages/packer.js to match the expected
+/// embedded path.
+pub fn package_request_handler<'a>(
+    incoming_prefix_name: String,
+    req_url: &'a str,
+) -> Option<EmbeddedFile> {
+    tracing::info!(
+        "[PackageRequestHandler] Received request for path: {}",
+        req_url
+    );
+    let request_path = req_url.into_string();
+
+    match Packages::get(
+        request_path
+            .replace(&incoming_prefix_name, "packages")
+            .strip_prefix("/")
+            .unwrap_or_else(|| &req_url),
+    ) {
+        Some(html_data) => Some(html_data),
+        None => None,
+    }
+}
