63f00359b269a21d9550320f6ceed05da0d1ff1c
Alexander Ewetumo <trinoxf@gmail.com>
2025-04-26 07:20:06 +0800

    ADD: macro for embedding

--- backends/foundation_jsmacros/src/embedders.rs
+++ backends/foundation_jsmacros/src/embedders.rs
@@ -1,8 +1,15 @@
 extern crate proc_macro;
+use std::{env, path::Path};
+
 use proc_macro::TokenStream;
 
 pub fn embed_file_on_struct(attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    let manifest_path = Path::new(&manifest_dir).join("Cargo.toml");
+
     println!("attr: \"{attrs}\"");
     println!("item: \"{item}\"");
+    println!("ManifestDir: \"{manifest_dir}\"");
+    println!("ManifestPath: \"{manifest_path:?}\"");
     item
 }
