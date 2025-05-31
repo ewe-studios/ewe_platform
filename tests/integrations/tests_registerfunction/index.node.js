958dfbcc5ae168539f13319b40698c1b7b24a0eb
Alexander Ewetumo <trinoxf@gmail.com>
2025-06-01 07:48:47 +0800

    ADD: rewriter wasm

--- tests/integrations/tests_registerfunction/index.node.js
+++ tests/integrations/tests_registerfunction/index.node.js
@@ -1,14 +1,12 @@
-const { describe, it} = require("node:test");
+const { describe, it } = require("node:test");
 const assert = require("node:assert");
 const fs = require("node:fs");
 
 const wasmBuffer = fs.readFileSync("./module.wasm");
 
-
 describe("Web WASM Module", () => {
-test("check we can decode a params IntArray", () => {
-  console.log(megatron);
-  assert.ok(true);
+  test("check we can decode a params IntArray", () => {
+    console.log(megatron);
+    assert.ok(true);
+  });
 });
-});
-
