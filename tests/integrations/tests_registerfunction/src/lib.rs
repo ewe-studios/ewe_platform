271a72cbac4b555e8f703e2f99c86247b551aeed
Alexander Ewetumo <trinoxf@gmail.com>
2025-06-01 07:48:47 +0800

    ADD: rewriter wasm

--- tests/integrations/tests_registerfunction/src/lib.rs
+++ tests/integrations/tests_registerfunction/src/lib.rs
@@ -8,18 +8,12 @@ use foundation_nostd::*;
 
 #[no_mangle]
 extern "C" fn main() {
-    let console_log = host_runtime::api_v1::register_function(
+    let _ = host_runtime::api_v1::register_function(
         r"
         function(message){
             console.log(message);
         }",
     );
-
-    console_log.invoke(&[Params::Text8("Hello from intro")]);
-
-    std::panic::set_hook(Box::new(move |e| {
-        console_log.invoke(&[Params::Text8(e.to_string().as_str())]);
-    }));
 
     let console_log_id = host_runtime::api_v2::preallocate_func_external_reference();
     let instructions = internal_api::create_instructions(100, 100);
@@ -32,10 +26,6 @@ extern "C" fn main() {
         }",
         )
         .expect("should encode correctly");
-
-    instructions
-        .invoke_no_return_function(console_log_id, Some(&[Params::Text8("Hello from intro")]))
-        .expect("should register call");
 
     host_runtime::api_v2::send_instructions(instructions.complete().expect("complete instruction"));
 }
