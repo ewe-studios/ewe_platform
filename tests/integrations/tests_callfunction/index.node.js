f60002d04745d2116fd1a458379d92d945def562
Alexander Ewetumo <trinoxf@gmail.com>
2025-05-31 19:00:29 +0800

    ADD: modules

---
+++ tests/integrations/tests_callfunction/index.node.js
@@ -0,0 +1,14 @@
+const { describe, it} = require("node:test");
+const assert = require("node:assert");
+const fs = require("node:fs");
+
+const wasmBuffer = fs.readFileSync("./module.wasm");
+
+
+describe("Web WASM Module", () => {
+test("check we can decode a params IntArray", () => {
+  console.log(megatron);
+  assert.ok(true);
+});
+});
+
