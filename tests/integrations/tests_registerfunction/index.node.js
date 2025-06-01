5399bb50d11edbad9d2205cccbd65cd46f70f125
Alexander Ewetumo <trinoxf@gmail.com>
2025-06-01 23:29:03 +0800

    ADD: use megatron for tests

--- tests/integrations/tests_registerfunction/index.node.js
+++ tests/integrations/tests_registerfunction/index.node.js
@@ -3,93 +3,23 @@ const assert = require("node:assert");
 const fs = require("node:fs");
 const path = require("node:path");
 
+const megatron = require("./megatron.js");
+
 const EXECUTING_DIR = path.dirname(__filename);
 
 const wasm_buffer = fs.readFileSync(path.join(EXECUTING_DIR, "./module.wasm"));
 
-function create_imports() {
-  let v2_imports = {
-    apply_instruction_calls: [],
-    dom_allocate_external_pointer_values: [],
-    object_allocate_external_pointer_values: [],
-    function_allocate_external_pointer_values: [],
-  };
-
-  v2_imports.apply_instructions = (
-    op_addr,
-    op_length,
-    text_addr,
-    text_length,
-  ) => {
-    v2_imports.apply_instructions.push([
-      op_addr,
-      op_length,
-      text_addr,
-      text_length,
-    ]);
-  };
-
-  v2_imports.function_allocate_external_pointer = () => {
-    v2_imports.function_allocate_external_pointer_values.push([
-      v2_imports.function_allocate_external_pointer_values.length,
-    ]);
-  };
-
-  v2_imports.dom_allocate_external_pointer = () => {
-    v2_imports.dom_allocate_external_pointer_values.push([
-      v2_imports.dom_allocate_external_pointer_values.length,
-    ]);
-  };
-
-  v2_imports.object_allocate_external_pointer = () => {
-    v2_imports.object_allocate_external_pointer_values.push([
-      v2_imports.object_allocate_external_pointer_values.length,
-    ]);
-  };
-
-  const v1_imports = {
-    drop_reference_calls: [],
-    js_invoke_function_calls: [],
-    js_register_function_calls: [],
-    js_unregister_function_calls: [],
-    js_register_function_at_calls: [],
-  };
-
-  v1_imports.drop_reference = function (external_id) {
-    v1_imports.drop_reference_calls.push(external_id);
-  };
-
-  v1_imports.js_invoke_function = function (
-    handler,
-    parameter_start,
-    parameter_length,
-  ) {
-    v1_imports.js_invoke_function_calls.push([
-      handler,
-      parameter_start,
-      parameter_length,
-    ]);
-  };
-
-  v1_imports.js_register_function = function (start, len, encoding) {
-    v1_imports.js_register_function_calls.push([start, len, encoding]);
-    return BigInt(v1_imports.js_register_function_calls.length);
-  };
-
-  v1_imports.js_unregister_function = function (handler) {
-    v1_imports.js_unregister_function_calls.push([handler]);
-  };
-
-  return { v1: v1_imports, v2: v2_imports };
-}
-
 describe("Megatron.registerfunction", async () => {
-  const imports = create_imports();
-  const { module, instance } = await WebAssembly.instantiate(wasm_buffer, {
-    ...imports,
+  const runtime = new megatron.MegatronMiddleware();
+  const wasm_module = await WebAssembly.instantiate(wasm_buffer, {
+    v1: runtime.v1_mappings,
+    v2: runtime.v2_mappings,
   });
+  runtime.init(wasm_module);
 
   describe("Validate::setup", () => {
+    const { module, instance } = wasm_module;
+
     it("able to validate module and instance", () => {
       assert.ok(module != undefined && module != null);
       assert.ok(instance != undefined && instance != null);
@@ -101,15 +31,11 @@ describe("Megatron.registerfunction", async () => {
   });
 
   describe("Validate::FuncRegistration", async () => {
+    const { module, instance } = wasm_module;
     instance.exports.main();
 
     it("validate register function was called", async () => {
-      imports.v1.js_register_function_at_calls.length == 1;
-      console.log("Imports: ", imports.v1.js_register_function_calls);
-      const [addr, length, encoding] = imports.v1.js_register_function_calls[0];
-      console.log("Register: ", addr, length, enc);
-      assert.equal(encoding, 8);
-      assert.equal(encoding, 71n);
+      assert.equal(runtime.function_heap.length(), 1);
     });
   });
 });
