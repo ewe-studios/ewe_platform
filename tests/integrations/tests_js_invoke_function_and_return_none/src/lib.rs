2d462d3d6ce6063c4dc4891ce2442443755ca045
Alexander Ewetumo <trinoxf@gmail.com>
2025-06-29 23:21:58 +0800

    ADD: confirm can return ReturnTypeId::None

---
+++ tests/integrations/tests_js_invoke_function_and_return_none/src/lib.rs
@@ -0,0 +1,31 @@
+#![allow(unused_imports)]
+
+use foundation_jsnostd::{
+    self, exposed_runtime, host_runtime, internal_api, ExternalPointer, Params,
+};
+
+use foundation_nostd::*;
+
+#[no_mangle]
+extern "C" fn main() {
+    let console_log = host_runtime::web::register_function(
+        r"
+        function(message){
+            console.log('Error occurred: ',message);
+        }",
+    );
+
+    std::panic::set_hook(Box::new(move |e| {
+        console_log.invoke_no_return(&[Params::Text8(e.to_string().as_str())]);
+    }));
+
+    let handler = host_runtime::web::register_function(
+        r"
+        function(v1){
+            this.mock.is_sample(v1);
+            return this.asNone();
+        }",
+    );
+
+    handler.invoke_for_none(&[Params::Bool(true)]);
+}
