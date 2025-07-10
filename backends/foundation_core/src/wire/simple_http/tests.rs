4f22bc8af5acb26791c5d18d0a9d03c1e7f08dd6
Alexander Ewetumo <trinoxf@gmail.com>
2025-07-10 21:29:16 +0800

    ADD: migrate tests

--- backends/foundation_core/src/wire/simple_http/tests.rs
+++ backends/foundation_core/src/wire/simple_http/tests.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-mod test_http_reader_compliance {
+mod test_http_reader_requests_compliance {
     use regex::Regex;
 
     use crate::io::ioutils;
@@ -15,6 +15,271 @@ mod test_http_reader_compliance {
         net::{TcpListener, TcpStream},
         thread,
     };
+
+    #[test]
+    fn request_finish_should_safe_to_finish_with_incomplete_put_request() {
+        let message = "\
+GET / HTTP/1.1
+Content-Length: 100
+
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_finish_should_unsafe_to_finish_with_incomplete_put_request() {
+        let message = "\
+PUT / HTTP/1.1
+Content-Length: 100
+
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_upgrade_post_request() {
+        let message = "\
+POST /demo HTTP/1.1
+Host: example.com
+Connection: Upgrade
+Upgrade: HTTP/2.0
+Content-Length: 15
+
+sweet post body\
+Hot diggity dogg
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_get_request() {
+        let message = "\
+GET /demo HTTP/1.1
+Host: example.com
+Connection: Upgrade
+Sec-WebSocket-Key2: 12998 5 Y3 1  .P00
+Sec-WebSocket-Protocol: sample
+Upgrade: WebSocket
+Sec-WebSocket-Key1: 4 @1  46546xW%0l 1 5
+Origin: http://example.com
+
+Hot diggity dogg
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_part_of_body_and_pausing() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection: upgrade
+Content-Length: 4
+Upgrade: ws
+
+abcdefgh
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_setting_flag_and_pausing() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection: upgrade
+Upgrade: ws
+
+
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_invalid_whitespace_token_with_connection_header_field_lenient() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection : upgrade
+Content-Length: 4
+Upgrade: ws
+
+abcdefgh
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_invalid_whitespace_token_with_connection_header_field() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection : upgrade
+Content-Length: 4
+Upgrade: ws
+
+abcdefgh
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_with_folding_and_lws_and_crlf() {
+        let message = "\
+GET /demo HTTP/1.1
+Connection: keep-alive, \r\n upgrade
+Upgrade: WebSocket
+
+Hot diggity dogg
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_with_folding_and_lws() {
+        let message = "\
+GET /demo HTTP/1.1
+Connection: keep-alive, upgrade
+Upgrade: WebSocket
+
+Hot diggity dogg
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_multiple_tokens_with_folding() {
+        let message = "\
+GET /demo HTTP/1.1
+Host: example.com
+Connection: Something,
+ Upgrade, ,Keep-Alive
+Sec-WebSocket-Key2: 12998 5 Y3 1  .P00
+Sec-WebSocket-Protocol: sample
+Upgrade: WebSocket
+Sec-WebSocket-Key1: 4 @1  46546xW%0l 1 5
+Origin: http://example.com
+
+Hot diggity dogg
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn request_connection_with_multiple_tokens() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection: close, token, upgrade, token, keep-alive
+
+
+";
+
+        // TODO
+    }
+
+    #[test]
+    fn connection_clrf_between_request_is_explicit_close() {
+        let message = "\
+POST / HTTP/1.1
+Host: www.example.com
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 4
+Connection: close
+
+q=42
+
+GET / HTTP/1.1        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn connection_no_carriage_return_in_request() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection: keep\ralive
+
+
+        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn connectioon_close_received() {
+        let message = "\
+PUT /url HTTP/1.1
+Connection: close
+
+
+        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn implicit_keep_alive_for_crlf_between_requests() {
+        let message = "\
+POST / HTTP/1.1
+Host: www.example.com
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 4
+
+q=42
+
+GET / HTTP/1.1
+        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn connection_reset_without_keep_alive() {
+        let message = "\
+PUT /url HTTP/1.0
+Content-Length: 0
+
+PUT /url HTTP/1.1
+Transfer-Encoding: chunked
+
+
+        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn connection_header_protocol_without_headers() {
+        let message = "\
+PUT /url HTTP/1.0
+
+PUT /url HTTP/1.1
+
+
+        ";
+
+        // TODO
+    }
+
+    #[test]
+    fn connection_header_keep_alive() {
+        let message = "\
+PUT /url HTTP/1.1\r
+Connection: keep-alive\r
+\r
+PUT /url HTTP/1.1\r
+Connection: keep-alive\r
+        ";
+
+        // TODO
+    }
 
     #[test]
     fn plain_text_content_with_headers() {
@@ -86,4 +351,9 @@ Hello world!";
         assert_eq!(request_parts, expected_parts);
         req_thread.join().expect("should be closed");
     }
+}
+
+#[cfg(test)]
+mod test_http_reader_response_compliance {
+    // TODO
 }
