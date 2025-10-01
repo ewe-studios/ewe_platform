dc06f222fbf5f4dbd9e97daedbdfe2f48d31fa43
Alexander Ewetumo <alex.ewetumo@gmail.com>
2025-10-01 12:26:08 +0800

    ADD: rewrite the lock for buffer byte reader

--- backends/foundation_core/src/io/ioutils/mod.rs
+++ backends/foundation_core/src/io/ioutils/mod.rs
@@ -1,7 +1,7 @@
 // use crate::compati::Mutex;
 use std::io::{BufRead, BufReader, BufWriter, Cursor, IoSlice, IoSliceMut, Read, Result, Write};
 use std::sync::atomic::AtomicPtr;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, LockResult, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 use derive_more::derive::From;
 
@@ -338,6 +338,7 @@ pub fn buffered_stream_with_capacity<T: Write + Read>(
 pub enum PeekError {
     ZeroLengthNotAllowed,
     NotSupported,
+    LockAcquisitionError,
     BiggerThanCapacity {
         requested: usize,
         buffer_capacity: usize,
@@ -525,8 +526,14 @@ pub const DEFAULT_READ_SIZE: usize = if cfg!(target_os = "espidf") {
 
 /// SharedPointerReader defines a shared buffer reader pointer that allows reading through
 /// a underlying buffered stream.
-#[derive(Clone)]
 pub struct SharedByteBufferStream<T: Read>(std::sync::Arc<std::sync::RwLock<ByteBufferPointer<T>>>);
+
+// Implement cloning for the [`SharedByteBufferStream`].
+impl<T: Read> Clone for SharedByteBufferStream<T> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
 
 // Constructors
 
@@ -630,20 +637,41 @@ impl<T: Read> SharedByteBufferStream<T> {
         binding.fill_up()
     }
 
+    // #[inline]
+    // pub fn peek(&self) -> std::io::Result<PeekState<'_>> {
+    //     let binding = self.0.read().expect("get lock");
+    //     binding.peek()
+    // }
+    //
+    // #[inline]
+    // pub fn peek_by<'a>(&'a self, by: usize) -> std::io::Result<PeekState<'a>> {
+    //     let binding = self.0.read().expect("get lock");
+    //     binding.peek_by(by)
+    // }
+
+    /// [`read`] returns a borrowed [`RwLockReadGuard`] which allows you access the internal
+    /// buffered byte reader for performing operations.
     #[inline]
-    pub fn peek(&self) -> std::io::Result<PeekState<'_>> {
-        let binding = self.0.read().expect("get lock");
-        binding.peek()
+    pub fn read(&self) -> LockResult<RwLockReadGuard<'_, ByteBufferPointer<T>>> {
+        self.0.read()
     }
 
+    /// [`write`] returns a borrowed [`RwLockWriteGuard`] which allows you access the internal
+    /// buffered byte reader for performing operations.
     #[inline]
-    pub fn peek_by<'a>(&'a self, by: usize) -> std::io::Result<PeekState<'a>> {
-        let binding = self.0.read().expect("get lock");
-        binding.peek_by(by)
+    pub fn write(&self) -> LockResult<RwLockWriteGuard<'_, ByteBufferPointer<T>>> {
+        self.0.write()
     }
+}
 
-    pub fn read_size<'a, 'b>(&'a mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        match self.peek_size(buf.len()) {
+impl<T: Read> PeekableReadStream for SharedByteBufferStream<T> {
+    fn peek(&mut self, buf: &mut [u8]) -> std::result::Result<usize, PeekError> {
+        let mut binding = self
+            .0
+            .write()
+            .map_err(|_| PeekError::LockAcquisitionError)?;
+
+        match binding.peek_size(buf.len()) {
             Ok(state) => match state {
                 PeekState::Request(data) => {
                     let ending = if buf.len() > data.len() {
@@ -657,351 +685,38 @@ impl<T: Read> SharedByteBufferStream<T> {
                     }
                     Ok(data.len())
                 }
-                PeekState::LessThanRequested => {
-                    let ending = if buf.len() > self.buffer.len() {
-                        self.buffer.len()
-                    } else {
-                        buf.len()
-                    };
-                    for (index, elem) in self.buffer[0..ending].iter().enumerate() {
-                        buf[index] = *elem
-                    }
-                    Ok(ending)
-                }
-                _ => return Ok(0),
-            },
-            Err(err) => Err(err),
-        }
-    }
-
-    /// [`peek_size`] returns a portion of the underlying buffer for the specified
-    /// size using a tight loop until the requested size is of data has being pulled
-    /// into the internal peek buffer for peeking .
-    ///
-    /// This moves forward the cursor forward until the requested size is achieved
-    /// and if the loop stops and the current buffer size does not match then
-    /// we return what's already acquired, so you need to be aware that this can happen
-    /// since generally it means we have reached EOF from the internal readers perspective.
-    #[inline]
-    pub fn peek_size<'a, 'b>(&'a mut self, size: usize) -> std::io::Result<PeekState<'a>> {
-        if size == 0 {
-            return Ok(PeekState::ZeroLengthInput);
-        }
-
-        loop {
-            let buffer_len = self.buffer.len();
-            let rem = size - buffer_len;
-
-            if rem < 0 {
-                break;
-            }
-
-            let state = self.peek_by(rem)?;
-            let read = match state {
-                PeekState::Request(po) => po.len(),
-                PeekState::Continue | PeekState::EndOfBuffered | PeekState::LessThanRequested => {
-                    continue;
-                }
-                PeekState::EndOfFile => {
-                    break;
-                }
-                _ => {
-                    unreachable!("Should never hit this types")
-                }
-            };
-
-            self.peek_pos += read;
-        }
-
-        let slice = &self.buffer[self.pos..self.peek_pos];
-        Ok(PeekState::Request(slice))
-    }
-
-    /// [`peek_until`] returns the peek state for the requested data until the delimiter is seen
-    /// at which point the underlying reference to the data is either shared in a
-    /// [`PeekState::Request`] if fully read else return [`PeekState::EndOfFile`] if the
-    /// source returns EOF or if the data available is less than requested.
-    ///
-    /// It runs in a tight loop and ensures to acquire as much data as needed.
-    #[inline]
-    pub fn peek_until<'a, 'b>(&'a mut self, signal: &'b [u8]) -> std::io::Result<PeekState<'a>> {
-        if signal.len() == 0 {
-            return Ok(PeekState::ZeroLengthInput);
-        }
-        loop {
-            let state = self.peek_by(signal.len())?;
-            match state {
-                PeekState::Request(data) => {
-                    if data == signal {
-                        break;
-                    }
-                }
-                PeekState::EndOfBuffered | PeekState::LessThanRequested => {
-                    // if at the end then return with EndOfFile
-                    if self.fill_up()? == 0 {
-                        return Ok(PeekState::EndOfFile);
-                    }
-                    continue;
-                }
-                PeekState::Continue => continue,
-                PeekState::EndOfFile => {
-                    return Ok(PeekState::EndOfFile);
-                }
-                _ => {
-                    unreachable!("Should never hit this types")
-                }
-            };
-
-            self.peek_pos += signal.len();
-        }
-
-        // account for the found block where we break and ensure we can capture
-        // the last peek correctly.
-        self.peek_pos += signal.len();
-        Ok(PeekState::Request(&self.buffer[self.pos..self.peek_pos]))
-    }
-
-    /// [`read_bytes_until`] reads the provided bytes into a Vec consuming read cursor, until it
-    /// finds the relevant else stopping.
-    ///
-    /// If the new line byte is not found then everything read till that point is appended to the
-    /// string and the cursor is consumed.
-    pub fn read_bytes_until<'a>(
-        &'a mut self,
-        target: &[u8],
-        buf: &mut Vec<u8>,
-    ) -> std::io::Result<usize> {
-        let read = match self.peek_until(target) {
-            Ok(inner) => match inner {
-                PeekState::Request(c) => {
-                    unsafe {
-                        buf.extend_from_slice(&c);
-                    };
-                    Ok(c.len())
-                }
-                PeekState::EndOfFile => Ok(0),
                 PeekState::ZeroLengthInput => Ok(0),
-                _ => unreachable!("Should never trigger"),
+                _ => unreachable!("We should never hit this state"),
             },
-            Err(err) => return Err(err),
-        };
-
-        match read {
-            Ok(inner) => {
-                if inner == 0 {
-                    let slice = &self.buffer[self.pos..self.peek_pos];
-                    let slice_length = slice.len();
-                    unsafe {
-                        buf.extend_from_slice(&slice);
-                    };
-
-                    self.skip();
-                    return Ok(slice_length);
-                }
-
-                self.skip();
-                return Ok(inner);
-            }
-            Err(err) => Err(err),
+            Err(err) => Err(PeekError::IOError(err)),
         }
     }
+}
 
-    /// [`read_line`] reads the provided line into a string and consumes the read data moving
-    /// the cusrsor forward.
-    ///
-    /// If the new line byte is not found then everything read till that point is appended to the
-    /// string and the cursor is consumed.
-    pub fn read_line<'a>(&'a mut self, buf: &mut String) -> std::io::Result<usize> {
-        const NEWLINE_SLICE: &[u8] = b"\n";
-        let read = match self.peek_until(NEWLINE_SLICE) {
-            Ok(inner) => match inner {
-                PeekState::Request(c) => {
-                    unsafe {
-                        let mut buf_vec = buf.as_mut_vec();
-                        buf_vec.extend_from_slice(&c);
-                    };
-                    Ok(c.len())
-                }
-                PeekState::EndOfFile => Ok(0),
-                PeekState::ZeroLengthInput => Ok(0),
-                _ => unreachable!("Should never trigger"),
-            },
-            Err(err) => return Err(err),
-        };
-
-        match read {
-            Ok(inner) => {
-                if inner == 0 {
-                    let slice = &self.buffer[self.pos..self.peek_pos];
-                    let slice_length = slice.len();
-
-                    unsafe {
-                        let mut buf_vec = buf.as_mut_vec();
-                        buf_vec.extend_from_slice(&slice);
-                    };
-
-                    self.skip();
-                    return Ok(slice_length);
-                }
-
-                self.skip();
-                return Ok(inner);
-            }
-            Err(err) => Err(err),
-        }
+impl<T: Read> std::io::Read for SharedByteBufferStream<T> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        let mut binding = self.write().expect("should unlock reader");
+        binding.read(buf)
     }
 
-    /// [`peek_line`] reads the provided line into a string without consuming the read content.
-    /// Allowing you to perform further operation on the data.
-    ///
-    /// If the newline byte is not found then it reads all the bytes into the provided buffer ontil
-    /// it hits EOF or EndOfFile but this is key, it won't move the cursor forward, just copies the
-    /// underlying data over.
-    pub fn peek_line<'a>(&'a mut self, buf: &mut String) -> std::io::Result<usize> {
-        const NEWLINE_SLICE: &[u8] = b"\n";
-        let read = match self.peek_until(NEWLINE_SLICE) {
-            Ok(inner) => match inner {
-                PeekState::Request(c) => {
-                    unsafe {
-                        let mut buf_vec = buf.as_mut_vec();
-                        buf_vec.extend_from_slice(&c);
-                    };
-                    Ok(c.len())
-                }
-                PeekState::EndOfFile => Ok(0),
-                PeekState::ZeroLengthInput => Ok(0),
-                _ => unreachable!("Should never trigger"),
-            },
-            Err(err) => return Err(err),
-        };
-
-        match read {
-            Ok(inner) => {
-                if inner == 0 {
-                    let slice = &self.buffer[self.pos..self.peek_pos];
-                    let slice_length = slice.len();
-
-                    unsafe {
-                        let mut buf_vec = buf.as_mut_vec();
-                        buf_vec.extend_from_slice(&slice);
-                    };
-
-                    return Ok(slice_length);
-                }
-
-                return Ok(inner);
-            }
-            Err(err) => Err(err),
-        }
+    fn read_vectored(&mut self, buf: &mut [IoSliceMut<'_>]) -> Result<usize> {
+        let mut binding = self.write().expect("should unlock reader");
+        binding.read_vectored(buf)
     }
 
-    /// [`peek_bytes_until`] reads the provided bytes into a Vec without consuming the read cursor.
-    /// Allowing you to perform further operation on the data.
-    ///
-    /// If the target byte is not found then it reads all the bytes into the provided buffer ontil
-    /// it hits EOF or EndOfFile but this is key, it won't move the cursor forward, just copies the
-    /// underlying data over.
-    pub fn peek_bytes_until<'a>(
-        &'a mut self,
-        target: &[u8],
-        buf: &mut Vec<u8>,
-    ) -> std::io::Result<usize> {
-        let read = match self.peek_until(target) {
-            Ok(inner) => match inner {
-                PeekState::Request(c) => {
-                    unsafe {
-                        buf.extend_from_slice(&c);
-                    };
-                    Ok(c.len())
-                }
-                PeekState::EndOfFile => Ok(0),
-                PeekState::ZeroLengthInput => Ok(0),
-                _ => unreachable!("Should never trigger"),
-            },
-            Err(err) => return Err(err),
-        };
-
-        match read {
-            Ok(inner) => {
-                if inner == 0 {
-                    let slice = &self.buffer[self.pos..self.peek_pos];
-                    let slice_length = slice.len();
-
-                    unsafe {
-                        buf.extend_from_slice(&slice);
-                    };
-
-                    return Ok(slice_length);
-                }
-
-                return Ok(inner);
-            }
-            Err(err) => Err(err),
-        }
+    fn read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
+        let mut binding = self.write().expect("should unlock reader");
+        binding.read_exact(buf)
     }
 
-    #[inline]
-    fn forward(&mut self) -> std::io::Result<PeekState<'_>> {
-        self.forward_by(1)
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
+        let mut binding = self.write().expect("should unlock reader");
+        binding.read_to_end(buf)
     }
 
-    #[inline]
-    fn forward_by(&mut self, by: usize) -> std::io::Result<PeekState<'_>> {
-        let buffer_length = self.buffer.len();
-        let new_peek = self.peek_pos + by;
-        if new_peek > buffer_length {
-            self.peek_pos = buffer_length;
-            Ok(PeekState::EndOfBuffered)
-        } else {
-            self.peek_pos = new_peek;
-            Ok(PeekState::Continue)
-        }
-    }
-
-    /// unnext_by moves your peek position backwards at which if it moves
-    /// all the way back, will forever stay at the last know position of
-    /// the actual data cursor.
-    #[inline]
-    fn unforward_by(&mut self, by: usize) -> std::io::Result<PeekState<'_>> {
-        let new_peek = self.peek_pos - by;
-        if new_peek <= self.pos {
-            self.peek_pos = self.pos;
-            Ok(PeekState::Continue)
-        } else {
-            self.peek_pos = new_peek;
-            Ok(PeekState::Continue)
-        }
-    }
-
-    /// consumes the amount of data that has been peeked over-so far, returning
-    /// that to the caller, this also moves the position of the data cursor
-    /// forward to the location of the skip cursor.
-    fn consume(&mut self) -> std::io::Result<Vec<u8>> {
-        let from = self.pos;
-        let until_pos = self.peek_pos;
-        if from == until_pos {
-            return Ok(vec![]);
-        }
-        let slice = &self.buffer[from..until_pos];
-        self.pos = self.peek_pos;
-
-        let mut slice_copy = vec![0; slice.len()];
-        slice_copy.truncate(0);
-        slice_copy.extend_from_slice(slice);
-
-        Ok(slice_copy)
-    }
-
-    /// skip the amount of data that has been peeked over-so far, returning
-    /// that to the caller, this also moves the position of the data cursor
-    /// forward to the location of the skip cursor.
-    fn skip(&mut self) {
-        let from = self.pos;
-        let until_pos = self.peek_pos;
-        if from == until_pos {
-            return;
-        }
-        self.pos = self.peek_pos;
+    fn read_to_string(&mut self, buf: &mut String) -> Result<usize> {
+        let mut binding = self.write().expect("should unlock reader");
+        binding.read_to_string(buf)
     }
 }
 
@@ -1565,58 +1280,6 @@ impl<T: Read> ByteBufferPointer<T> {
 impl<T: Read> Read for ByteBufferPointer<T> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         self.read_size(buf)
-    }
-}
-
-impl<T: Read> std::io::Read for Arc<Mutex<ByteBufferPointer<T>>> {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let mut binding = self.lock().expect("should unlock reader");
-        binding.read(buf)
-    }
-
-    fn read_vectored(&mut self, buf: &mut [IoSliceMut<'_>]) -> Result<usize> {
-        let mut binding = self.lock().expect("should unlock reader");
-        binding.read_vectored(buf)
-    }
-
-    fn read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
-        let mut binding = self.lock().expect("should unlock reader");
-        binding.read_exact(buf)
-    }
-
-    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> Result<usize> {
-        let mut binding = self.lock().expect("should unlock reader");
-        binding.read_to_end(buf)
-    }
-
-    fn read_to_string(&mut self, buf: &mut String) -> Result<usize> {
-        let mut binding = self.lock().expect("should unlock reader");
-        binding.read_to_string(buf)
-    }
-}
-
-impl<T: Read> PeekableReadStream for Arc<Mutex<ByteBufferPointer<T>>> {
-    fn peek(&mut self, buf: &mut [u8]) -> std::result::Result<usize, PeekError> {
-        let mut binding = self.lock().unwrap();
-        match binding.peek_size(buf.len()) {
-            Ok(state) => match state {
-                PeekState::Request(data) => {
-                    let ending = if buf.len() > data.len() {
-                        data.len()
-                    } else {
-                        buf.len()
-                    };
-
-                    for (index, elem) in data[0..ending].iter().enumerate() {
-                        buf[index] = *elem
-                    }
-                    Ok(data.len())
-                }
-                PeekState::ZeroLengthInput => Ok(0),
-                _ => unreachable!("We should never hit this state"),
-            },
-            Err(err) => Err(PeekError::IOError(err)),
-        }
     }
 }
 
