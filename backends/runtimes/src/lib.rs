55026fc3f8e9ce99d57bcc74d8ac26aee7d2f4d1
Alexander Ewetumo <alex.ewetumo@gmail.com>
2025-12-13 02:02:07 +0800

    ADD: ensure compression

--- backends/runtimes/src/lib.rs
+++ backends/runtimes/src/lib.rs
@@ -4,6 +4,7 @@ pub mod js_runtimes {
     use super::*;
 
     #[derive(EmbedFileAs, Default)]
+    #[gzip_compression]
     #[source = "../../assets/jsruntime/megatron.js"]
     pub struct JSHostRuntime;
 }
