b5588a2d9024e2480f23c9dfd4da8fde94e0a912
Alexander Ewetumo <alex.ewetumo@gmail.com>
2026-01-10 19:38:07 +0800

    ADD: ensure we pull llamacpp

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -2,3 +2,4 @@
 
 git submodule update --init --recursive tools/emsdk
 git submodule update --init --depth=1 tools/dawn
+git submodule update --init --recursive infrastructure/llama-bindings/llama.cpp
