c8f876086d77784ee601c2235bead2bded107ddc
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-02-19 15:24:08 +0800

    UPT: spec update and moves

--- tests/backends/foundation_nostd/integrations/mod.rs
+++
@@ -1,8 +0,0 @@
-#![cfg(test)]
-
-pub mod barrier_debug;
-pub mod integration_tests;
-
-// wasm_tests is conditional
-#[cfg(target_arch = "wasm32")]
-pub mod wasm_tests;
--- tests/backends/foundation_nostd/mod.rs
+++ tests/backends/foundation_nostd/mod.rs
@@ -1,3 +1,4 @@
 #![cfg(test)]
 
+pub mod benchmarks;
 pub mod integrations;
