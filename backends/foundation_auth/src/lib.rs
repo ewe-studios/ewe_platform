6ad07ca42debae8fb44a2ff89a59fc69f37a0162
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-03-17 21:10:12 +0800

    ADD: rewire definitions

--- backends/foundation_auth/src/lib.rs
+++ backends/foundation_auth/src/lib.rs
@@ -1,10 +1,13 @@
+use std::error::Error;
+
 use derive_more::From;
+use foundation_core::{valtron::StreamIterator, wire::simple_http::client::Cookie};
 use zeroize::Zeroizing;
 
 #[derive(From, Clone)]
-pub struct Confidential(Zeroizing<String>);
+pub struct ConfidentialText(Zeroizing<String>);
 
-impl Confidential {
+impl ConfidentialText {
     #[must_use]
     pub fn new(value: String) -> Self {
         Self(Zeroizing::new(value))
@@ -16,13 +19,13 @@ impl Confidential {
     }
 }
 
-impl core::fmt::Debug for Confidential {
+impl core::fmt::Debug for ConfidentialText {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{self:}")
     }
 }
 
-impl core::fmt::Display for Confidential {
+impl core::fmt::Display for ConfidentialText {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let value = &(*self.0);
         let first_char = value.chars().next().unwrap_or('*');
@@ -33,15 +36,37 @@ impl core::fmt::Display for Confidential {
 }
 
 #[derive(From, Debug, Clone)]
-pub struct AuthenticatedCredentials {
+pub struct OAuthCredential {
     pub expires: f64,
-    pub access_token: Confidential,
-    pub refresh_token: Option<Confidential>,
+    pub access_token: ConfidentialText,
+    pub refresh_token: Option<ConfidentialText>,
+}
+
+#[derive(From, Debug, Clone)]
+pub struct JwtCredential {
+    pub expires: f64,
+    pub token: ConfidentialText,
+}
+
+#[derive(From, Debug, Clone)]
+pub struct SessionCredential {
+    pub expires: f64,
+    pub session_id: String,
+    pub token: ConfidentialText,
+    pub cookie: Option<Cookie>,
+}
+
+#[derive(From, Debug, Clone)]
+pub enum Authenticated {
+    OAuth(OAuthCredential),
+    JWt(JwtCredential),
+    Session(SessionCredential),
 }
 
 #[derive(From, Debug, Clone)]
 pub enum AuthCredential {
-    SecretOnly(Confidential),
+    OAuth(OAuthCredential),
+    SecretOnly(ConfidentialText),
 
     EmailAuth {
         email: String,
@@ -49,17 +74,73 @@ pub enum AuthCredential {
 
     UsernameAndPassword {
         username: String,
-        password: Confidential,
+        password: ConfidentialText,
     },
 
-    OAuthAuth(AuthenticatedCredentials),
-
     ClientSecret {
-        client_id: Confidential,
-        client_secret: Confidential,
+        client_id: ConfidentialText,
+        client_secret: ConfidentialText,
     },
 }
 
-pub trait OAuthProviderEndpoint {
-    fn login(&self, credential: AuthCredential);
+#[derive(From)]
+pub enum AuthenticationErrors {
+    InvalidCredentials,
+    RequestErrors,
+    FailedToConnect,
+    InvalidEndpoint,
+}
+
+impl core::fmt::Display for AuthenticationErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AuthenticationErrors::InvalidCredentials => write!(f, "Invalid credentials"),
+            AuthenticationErrors::RequestErrors => write!(f, "Request errors"),
+            AuthenticationErrors::FailedToConnect => write!(f, "Failed to connect"),
+            AuthenticationErrors::InvalidEndpoint => write!(f, "Invalid endpoint"),
+        }
+    }
+}
+
+impl core::fmt::Debug for AuthenticationErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        core::fmt::Display::fmt(self, f)
+    }
+}
+
+impl Error for AuthenticationErrors {}
+
+pub type AuthenticationResult<T> = std::result::Result<T, AuthenticationErrors>;
+
+#[derive(From, Debug, Clone)]
+pub enum OnAuthData {
+    OAuth {
+        url: ConfidentialText,
+        instructions: Option<String>,
+    },
+
+    OnTwoFactor {
+        // location to send two factor token into.
+        url: ConfidentialText,
+    },
+}
+
+#[derive(From, Debug, Clone)]
+pub enum AuthenticationStates {
+    Connecting,
+    Prompting(Option<OnAuthData>),
+    Progressing(Option<String>),
+    Done,
+    Aborted,
+}
+
+pub trait AuthProviderEndpoint {
+    /// Attempts to log in using the provided credentials.
+    ///
+    /// # Errors
+    ///
+    /// Returns an [`AuthenticationErrors`] if authentication fails or an error occurs during the process.
+    fn login<T>(&self, credential: AuthCredential) -> AuthenticationResult<T>
+    where
+        T: StreamIterator<Authenticated, AuthenticationStates>;
 }
