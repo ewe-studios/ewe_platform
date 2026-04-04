2654381cf52405cd7ff7e1c074f4537e02843b56
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-04-04 17:52:44 +0800

    FIX: build setup

--- backends/foundation_core/tests/event_source/mod.rs
+++ backends/foundation_core/tests/event_source/mod.rs
@@ -3,7 +3,6 @@
 mod consumer_tests;
 mod core_tests;
 mod error_tests;
-mod parser_tests;
 mod reconnecting_task_tests;
 mod response_tests;
 mod task_tests;
--- tests/backends/foundation_core/event_source/mod.rs
+++ tests/backends/foundation_core/event_source/mod.rs
@@ -1,5 +1,6 @@
 #![cfg(test)]
 
 mod consumer_integration_tests;
+mod parser_tests;
 mod reconnecting_task_integration_tests;
 mod task_integration_tests;
