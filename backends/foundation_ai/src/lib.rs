7b25c0261958cb036ffadf269efaefc2688318a2
alex.ewetumo@gmail.com <Ewetumo Alexander>
2026-06-28 22:33:20 +0800

    FIX: wrong initialization

--- backends/foundation_ai/src/lib.rs
+++ backends/foundation_ai/src/lib.rs
@@ -1,17 +1,7 @@
 #![allow(clippy::too_many_lines)]
-#![allow(clippy::missing_errors_doc)]
-#![allow(clippy::missing_panics_doc)]
-#![allow(clippy::default_trait_access)]
+#![allow(clippy::too_many_arguments)]
 
-#![allow(clippy::items_after_statements)]
-#![allow(clippy::match_same_arms)]
-#![allow(clippy::single_match_else)]
-#![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_possible_wrap)]
-#![allow(clippy::cast_sign_loss)]
-#![allow(clippy::cast_precision_loss)]
 extern crate lazy_regex;
-// extern crate lazy_static;
 
 pub mod agentic;
 pub mod backends;
